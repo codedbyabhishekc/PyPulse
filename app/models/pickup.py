012fd32d380c2db501e6e513b4411e1e1697a74e
codeasign <mail2abhishekch@gmail.com>
2026-04-16 21:10:12 +0530

    Added Check

--- app/models/pickup.py
+++ app/models/pickup.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 class PickupCreate(BaseModel):
     sender_id: int   # was int
-    receiver_name: int
+    tiuger: int
     phone: int
     email_address: str       # ADD NEW FIELD
     area: str
-    counry:str
+    gender:str
     current_location: str
