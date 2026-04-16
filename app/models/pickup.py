86a799c3046220231430a78ccf41404bbdda1bd1
codeasign <mail2abhishekch@gmail.com>
2026-04-16 20:33:23 +0530

    Tried again

--- app/models/pickup.py
+++ app/models/pickup.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 
 class PickupCreate(BaseModel):
-    sender_id: str   # was int
-    receiver_name: str
+    sender_id: int   # was int
+    receiver_name: int
     phone: int
     email_address: str       # ADD NEW FIELD
     area: str
